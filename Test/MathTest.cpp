fe6ee837933262531d26e4c98760e32af9132c52
kai <vnkmxx@Gmail.com>
2022-02-03 15:31:26 +0800

    ✨ Test and Game

---
+++ Test/MathTest.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+
+int add(int lhs, int rhs) {
+    return lhs + rhs; 
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    return 0;
+}
+
+TEST(sum, simple)
+{
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+}
+
+TEST(sum, ok)
+{
+    EXPECT_EQ(add(222, 3333), 3555);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+}
+
+TEST(sum, hard)
+{
+    EXPECT_EQ(add(222, 3333), 3555);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
+}
